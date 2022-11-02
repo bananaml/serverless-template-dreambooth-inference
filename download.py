75c572d24fc2bdef3fdc9ec4e9c7823f42c45478
Sahil Chaudhary <sahil280114@gmail.com>
2022-11-02 19:41:04 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,25 +1,21 @@
 # In this file, we define download_model
 # It runs during container build time to get model weights built into the container
 
-from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
+import boto3
 import os
 
 def download_model():
-    # do a dry run of loading the huggingface model, which will download weights at build time
-    #Set auth token which is required to download stable diffusion model weights
-    HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
-
-    lms = LMSDiscreteScheduler(
-        beta_start=0.00085, 
-        beta_end=0.012, 
-        beta_schedule="scaled_linear"
-    )
-
-    model = StableDiffusionPipeline.from_pretrained(
-        "CompVis/stable-diffusion-v1-4", 
-        scheduler=lms,
-        use_auth_token=HF_AUTH_TOKEN
-    )
+    # Download the weights from s3 (can be changed to download weights from any cloud)
+    os.makedirs("dreambooth_weights/")
+    s3 = boto3.resource(service_name='s3', region_name='AWS_REGION', aws_access_key_id='AWS_ACCESS_KEY', aws_secret_access_key='AWS_SECRET_ACCESS_KEY')
+    bucket = s3.Bucket("BUCKET_NAME")
+    for obj in bucket.objects.filter(Prefix="FOLDER_NAME"):
+        target = os.path.join("dreambooth_weights/", os.path.relpath(obj.key, "FOLDER_NAME"))
+        if not os.path.exists(os.path.dirname(target)):
+            os.makedirs(os.path.dirname(target))
+        if obj.key[-1] == '/':
+            continue
+        bucket.download_file(obj.key, target)
 
 if __name__ == "__main__":
     download_model()
